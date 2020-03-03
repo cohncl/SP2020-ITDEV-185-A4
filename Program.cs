99eff3d4d9d303b9a94fb1332a28e24848a5c50e
charles-cohn <cohncl@gmatc.matc.edu>
2020-03-02 23:09:56 -0600

    Homework submission for assignment 4

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace YieldExample
+{
+    class Program
+    {
+        static System.Collections.Generic.IEnumerable<int> GetList(int count)
+        {
+            // Fill item with int value
+            for (int i = 1; i < count + 1; i++)
+                yield return i;
+        }
+        static void Main(string[] args)
+        {
+            int l = 0;
+            Console.WriteLine("How many items do you want in your list?");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            foreach (var item in GetList(x))
+            {
+                l++;
+                Console.WriteLine($"item {l} = " + item.ToString());
+            }
+
+            Console.WriteLine("\nPress any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
